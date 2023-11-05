3b920dbcd5afe5dfa4b16f012241a07141ccc3d1
Martin <tucek.martin@gmail.com>
2023-11-05 12:36:27 +0100

    Add commit

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ from classes.Menu import Menu
 from classes.Sound import Sound
 from entities.Mario import Mario
 
-
 windowSize = 640, 480
-
 
 def main():
     pygame.mixer.pre_init(44100, -16, 2, 4096) # Set audio optons
